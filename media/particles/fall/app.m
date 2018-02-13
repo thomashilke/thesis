541eb4fe8572db1d69b22074c8f4084bd86d746a
Thomas Foetisch <thomas.foetisch@epfl.ch>
2018-02-13 17:07:57 +0100

    Transport et dissolution de populations, intro et modele

--- media/particles/fall/app.m
+++ media/particles/fall/app.m
@@ -4,7 +4,7 @@ f = fopen("results.tex", "w");
 for v0 = [0, 2.0, 3.0]
   fprintf(f, "$v_0 = \\num{%g}$\\\\\n", v0)
   for r0 = [40e-6, 60e-6, 80e-6]
-    x = depth(r0, v0, [0.5e-3, 0.01, 0.1, 1], parameters());
+    x = depth(r0, v0, [2e-3, 0.01, 0.1, 1], parameters());
     fprintf(f, "\\hspace{1em} $r_0 = \\num{%g}$ & %e & %e & %e & %e \\\\\n", r0*1e6, x)
   end
 end
--- media/particles/fall/depth.m
+++ media/particles/fall/depth.m
@@ -1,4 +1,4 @@
-function [x, xapp, v0eq] = depth(r0, v0, mu, param)
+function [x, xapp, v0eq, reynolds] = depth(r0, v0, mu, param)
   p = param;
   x =   p.g * (p.alumina_density - p.electrolyte_density) ./ ...
         (p.dissolution_rate * (  18.0 * mu  ...
@@ -15,4 +15,6 @@ function [x, xapp, v0eq] = depth(r0, v0, mu, param)
         (  18.0 * mu ) .* r0.^2;
 
   v0eq = p.g / (4.0 * p.dissolution_rate) * (p.alumina_density - p.electrolyte_density)/p.alumina_density * r0.^2;
+
+  reynolds = 4/9 * p.g * p.electrolyte_density * (p.electrolyte_density - p.alumina_density) * r0.^3 ./ mu.^2;
 end
